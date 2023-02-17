20560ede32c22af462d83c122fe97f5f6fc5cc99
whyleonardo <pleasefloyd73@gmail.com>
2023-02-17 00:02:29 -0300

    header styling and structure

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const title = document.getElementById('title')
+const mobileMenu = document.getElementById('mobile-menu')
+const openMenuButton = document.getElementById('open-menu-button')
+const closeMenuButton = document.getElementById('close-menu-button')
+
+openMenuButton.addEventListener('click', () => {
+  !mobileMenu.classList.contains('isOpen') &&
+    mobileMenu.classList.add('isOpen')
+})
+
+closeMenuButton.addEventListener('click', () => {
+  mobileMenu.classList.contains('isOpen') &&
+    mobileMenu.classList.remove('isOpen')
+})
