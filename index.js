900b516e0850dfbc8c00dc07cec5a86b9d803c11
whyleonardo <pleasefloyd73@gmail.com>
2023-02-26 12:37:02 -0300

    set hidden overflow when mobile menu is open

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const mobileMenu = document.getElementById('mobile-menu')
 const openMenuButton = document.getElementById('open-menu-button')
 const closeMenuButton = document.getElementById('close-menu-button')
+const html = document.querySelector('html')
 
 openMenuButton.addEventListener('click', () => {
-  !mobileMenu.classList.contains('isOpen') &&
+  if (!mobileMenu.classList.contains('isOpen')) {
     mobileMenu.classList.add('isOpen')
+    html.style.overflowY = 'hidden'
+  }
 })
 
 closeMenuButton.addEventListener('click', () => {
-  mobileMenu.classList.contains('isOpen') &&
+  if (mobileMenu.classList.contains('isOpen')) {
     mobileMenu.classList.remove('isOpen')
+    html.style.overflowY = 'visible'
+  }
 })
